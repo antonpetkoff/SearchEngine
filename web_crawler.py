00d42e294b1e6bd8a628f85e7188672fa8ad6ce1
tonynho <tonynho010@gmail.com>
2014-11-24 21:38:57 +0200

    recursion

--- web_crawler.py
+++ web_crawler.py
@@ -1,30 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
 class WebCrawler:
 
     def __init__(self):
         self.scanned_urls = []
+        self.base_url = ""
+
+    def is_outgoing(self, url):
+        if self.base_url in url:
+            return False
+        return True
+
+    def prepare_link(self, url, href):
+        return urljoin(url, href)
 
     def scan_page(self, url):
+        if url in self.scanned_urls:
+            return
+
+        print(url)
+        self.scanned_urls.append(url)
+
         r = requests.get(url)
         soup = BeautifulSoup(r.text)
 
         for link in soup.find_all('a'):
             href = link.get('href')
-            if href not in self.scanned_urls:
-                if href.startswith("http", 0, 4):
-                    self.scanned_urls.append(href)
-                    print(href)
-                else:
-                    self.scanned_urls.append(url + href)
-                    print(url + href)
+            new_link = self.prepare_link(url, href)
+            if not self.is_outgoing(url):
+                self.scan_page(new_link)
 
 
 def main():
     crawler = WebCrawler()
-    crawler.scan_page("http://hackbulgaria.com")
+    crawler.base_url = "http://hackbulgaria.com/"
+    crawler.scan_page("http://hackbulgaria.com/")
 
 
 if __name__ == '__main__':
